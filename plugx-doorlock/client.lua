25c4bfd12218f84dbb8b619b9d8ae2d01748b03c
PlugX Scripts <plugxsoftware26@gmail.com>
2025-12-16 08:53:04 -0600

    Delete plugx-doorlock directory

--- plugx-doorlock/client.lua
+++
@@ -1,7 +0,0 @@
-CreateThread(function()
-    while true do
-        local hour = GetClockHours()
-        TriggerServerEvent('plugx-doorlock:updateTime', hour)
-        Wait(3000) -- 10 seconds
-    end
-end)
